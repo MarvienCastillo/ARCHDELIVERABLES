d17bc907903d6a089a2f82a1fc5f8aed4ed41fcc
Marvien Castillo <160254067+MarvienCastillo@users.noreply.github.com>
2025-09-23 19:40:43 +0800

    with /get

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Video = require('../models/video'); 
+const Video = require('./models/video'); 
 
 // express app
 const app = express();
@@ -10,22 +10,10 @@ mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(3000, () => console.log('Server is running on port 3000')))
     .catch(err => console.log(err));
 
-// CRUD Functions
-
+// CRUD operation API
 
 // create video to the database
 app.get('/create',(req,res) => {
-    // const video = new Video({
-    //     title: 'ARCHers Recap March 17-28, 2025 | Archers Network',
-    //     date:   new Date("2025-04-05"),
-    //     description: `In this ARCHers Recap: The Legislative Assembly probes potential amendments to the DLSU Student Handbook, MANIBELA held a three-day transport strike against inaccurate PTMP consolidation figures, and DLSU advances campus renovations with new elevator construction. 
-    //                 ARCHers Recap features the latest university news and events, as well as the important national stories for the Lasallian community.
-    //                 #ARCHNews #DLSUNews #PHNews #ARCHersRecap #ARCH #ArchersNetwork`,
-    //     link: 'https://youtu.be/6KvyeW568wQ?si=63OSwFrIaJYu2Hws',
-    //     channel: 'Archers Recap',
-    //     series: 'NCA'
-    // })
-
     const video = new Video({
         title: 'New Title',
         date:   new Date("2025-04-04"),
@@ -33,8 +21,8 @@ app.get('/create',(req,res) => {
                     ARCHers Recap features the latest university news and events, as well as the important national stories for the Lasallian community.
                     #ARCHNews #DLSUNews #PHNews #ARCHersRecap #ARCH #ArchersNetwork`,
         link: 'https://youtu.be/6KvyeW568wQ?si=63OSwFrIaJYu2Hws',
-        channel: 'Archers Recap',
-        series: 'NCA'
+        channel: 'NCA',
+        series: 'Archers Recap'
     })
     
 
@@ -53,7 +41,7 @@ app.get('/update', async (req, res) => {
     const update = await Video.findByIdAndUpdate(
       '68d157f3023e3e1d9becd6f9',
       { title: 'New Title' },
-      { new: true } // return the updated document
+      { new: true } 
     );
 
     if (!update) {
@@ -79,14 +67,17 @@ app.get('/delete', async (req,res)=>{
             return res.status(404).send('Video not found');
         }
         console.log('Successful');
-        res.send(`Updated video: ${udeleteID.id}`);
+        res.send(`Updated video: ${deleteID.id}`);
     } catch(error){
         res.status(500).send('Delete error');
     }
 
 })
 // get video data
-
+app.get('/get', async (req,res)=>{
+    const get = await Video.find();
+    res.send(get);
+})
 
 // select all videos from a specific video series
 app.get('/select-all', async (req,res)=>{
